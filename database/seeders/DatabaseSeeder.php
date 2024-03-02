cd4fbed029d1820d062d54faa68ceb72856ad294
Iovanni <salcedofranco23@gmail.com>
2024-03-02 20:00:33 -0300

    corregido factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,19 +10,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::beginTransaction();
-
-        // desactiva fk
-        DB::statement('
-            DO $$
-            DECLARE
-                r RECORD;
-            BEGIN
-                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-                    EXECUTE \'ALTER TABLE \' || quote_ident(r.tablename) || \' DISABLE TRIGGER ALL\';
-                END LOOP;
-            END $$;
-        ');
+        // DB::beginTransaction();
 
         // seeders
         $this->call([
@@ -34,18 +22,6 @@ class DatabaseSeeder extends Seeder
             HistorialSeeder::class
         ]);
 
-        // activa fk
-        DB::statement('
-            DO $$
-            DECLARE
-                r RECORD;
-            BEGIN
-                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-                    EXECUTE \'ALTER TABLE \' || quote_ident(r.tablename) || \' ENABLE TRIGGER ALL\';
-                END LOOP;
-            END $$;
-        ');
-
-        DB::commit();
+        // DB::commit();
     }
 }
