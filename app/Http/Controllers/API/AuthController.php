c3a87b6d9d30959abd081d79e8ee39c23eedff54
Iovanni <salcedofranco23@gmail.com>
2024-03-10 20:16:20 -0300

    futbol 6

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
@@ -11,90 +12,97 @@ class AuthController extends Controller
 {
     // Registro User
     public function register(Request $request){
-        // valida request
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|confirmed'
-        ]);
+        try{
+            // valida request
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed'
+            ]);
 
-        // excepciones *
-        // mensajes
-        // bail
+            // inserta user
+            $pass = Hash::make($request->password);
+            $user = new User();
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->password = $pass;
+            $user->save();
 
-        // inserta user
-        $pass = Hash::make($request->password);
-        $user = new User();
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->password = $pass;
-        $user->save();
-
-        // response
-        return response()->json([
-            'status' => 200,
-            'errors' => [], 
-            'message' => 'Registro de user exitoso',
-            'data' => [
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => $pass
-            ]                  
-        ], 200);
+            // response
+            return response()->json([
+                'status' => 200,
+                'errors' => [], 
+                'message' => 'Registro de user exitoso',
+                'data' => [
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => $pass
+                ]                  
+            ], 200);
+        
+        // captura exception
+        }catch(ValidationException $e){
+            // manejo de mensajes
+            $errors = $e->validator->getMessageBag();
+            // response
+            return response()->json([
+                'status' => 422,
+                'errors' => $errors->keys(), 
+                'message' => 'Error de validación',
+                'data' => $errors->first()
+            ], 422);
+        }
     }
 
 
     // Login User
     public function login(Request $request){
-        // valida request
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
-        
-        // excepciones *
-        // mensajes
-        // bail
+        // try
+            // valida request
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+                    
+            // compara con bd
+            $user = User::where('email', '=', $request->email)->first();
+            // si existe
+            if(isset($user->id)){
+                // todo ok
+                if(Hash::check($request->password, $user->password)){ 
+                    // crea token
+                    $token = $user->createToken("auth_token")->plainTextToken;
+                    // response
+                    return response()->json([
+                        'status' => 200,
+                        'errors' => [], 
+                        'message' => 'User logeado exitosamente',
+                        'data' => [
+                            'acces_token' => $token
+                        ]                  
+                    ], 200);
 
-        // compara con bd
-        $user = User::where('email', '=', $request->email)->first();
-        // si existe
-        if(isset($user->id)){
-            // todo ok
-            if(Hash::check($request->password, $user->password)){ 
-                // crea token
-                $token = $user->createToken("auth_token")->plainTextToken;
-                // response
-                return response()->json([
-                    'status' => 200,
-                    'errors' => [], 
-                    'message' => 'User logeado exitosamente',
-                    'data' => [
-                        'acces_token' => $token
-                    ]                  
-                ], 200);
-
-            // contraseña incorrecta
+                // contraseña incorrecta
+                }else{ 
+                    // response
+                    return response()->json([
+                        'status' => 404,
+                        'errors' => [], 
+                        'message' => 'Contraseña incorrecta',
+                        'data' => []                  
+                    ], 404);
+                }
+            
             }else{ 
+                // si no existe
                 // response
                 return response()->json([
                     'status' => 404,
                     'errors' => [], 
-                    'message' => 'Contraseña incorrecta',
+                    'message' => 'User no registrado',
                     'data' => []                  
                 ], 404);
-            }
-        
-        }else{ 
-            // si no existe
-            // response
-            return response()->json([
-                'status' => 404,
-                'errors' => [], 
-                'message' => 'User no registrado',
-                'data' => []                  
-            ], 404);
-        }        
+            }        
     }
 
 
